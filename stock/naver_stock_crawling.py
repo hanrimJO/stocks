3a0a29485d5b56d76fdde2df16e79ce6de287c34
hanrimJO <whgksfla@gmail.com>
2019-12-20 19:07:18 +0900

    종목명, 현재주가, 종목별 url코드 크롤링

--- stock/naver_stock_crawling.py
+++ stock/naver_stock_crawling.py
@@ -1,15 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get('https://finance.naver.com/item/sise.nhn?code=005930')
-# print(html.text)
 
-soup = BeautifulSoup(html.text, 'html.parser')
+def sise():
+    html = requests.get('https://finance.naver.com/item/sise.nhn?code=005930')
+    # print(html.text)
+
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    stock_now = soup.select('#_nowVal')
+
+    for stock in stock_now:
+        print(stock.text)
 
 
-stock = soup.select('table.type2')
+def stock_list():
+    html = requests.get('https://finance.naver.com/sise/sise_market_sum.nhn')
+    # print(html.text)
+    soup = BeautifulSoup(html.text, 'html.parser')
 
-stock_now = soup.select('#_nowVal')
+    stock_tag = soup.select('table > tbody > tr > td:nth-child(2) > a')
 
-for stock in stock_now:
-    print(stock.text)
+    stock_now = soup.select('table > tbody > tr > td:nth-child(3)')
+
+    # print(stock_now)
+
+    num = 0
+    for tags in stock_tag:
+        num += 1
+        if num == 11:
+            break
+        company_name = tags.text
+        company_code = tags['href'].split('=')[1]
+        print(company_name, company_code)
+
+    num = 0
+    for tags in stock_now:
+        num += 1
+        if num == 11:
+            break
+        company_stock = tags.text
+        print(company_stock)
+
+
+
+
+stock_list()
