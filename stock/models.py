b752453e1a780a1c7f8808e87f9116db3cd818cf
hanrimJO <whgksfla@gmail.com>
2019-12-23 12:32:37 +0900

    크롤링 데이터와 모델 연결

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.IntegerField(max_length=50)
+    user_name = models.CharField(max_length=100)
+    user_email = models.CharField(max_length=200)
+    user_phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return [self.user_id, self.user_email, self.user_name, self.user_phone]
+
+
+class Company(models.Model):
+    company_code = models.IntegerField(max_length=100)
+    company_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return [self.company_name, self.company_code]
+
+
+class Stock(models.Model):
+    company_name = models.ForeignKey(Company.company_name, on_delete=models.CASCADE)
+    company_code = models.ForeignKey(Company.company_code, on_delete=models.CASCADE)
+    saved_time = models.DateTimeField(auto_now_add=True)
+    saved_day = models.DateField(auto_now_add=True)
+    company_stock = models.CharField(max_length=200)
+
+    def __str__(self):
+        return [self.company_name, self.company_stock, self.company_stock, self.saved_time, self.saved_day]
+
+
+
--- stock/naver_stock_crawling.py
+++ stock/naver_stock_crawling.py
@@ -1,20 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from .models import *
 
 
-def sise():
-    html = requests.get('https://finance.naver.com/item/sise.nhn?code=005930')
-    # print(html.text)
+# def sise():
+#     html = requests.get('https://finance.naver.com/item/sise.nhn?code=005930')
+#     # print(html.text)
+#
+#     soup = BeautifulSoup(html.text, 'html.parser')
+#
+#     stock_now = soup.select('#_nowVal')
+#
+#     for stock in stock_now:
+#         print(stock.text)
 
-    soup = BeautifulSoup(html.text, 'html.parser')
 
-    stock_now = soup.select('#_nowVal')
-
-    for stock in stock_now:
-        print(stock.text)
-
-
-def stock_list():
+def stock_price():
     html = requests.get('https://finance.naver.com/sise/sise_market_sum.nhn')
     # print(html.text)
     soup = BeautifulSoup(html.text, 'html.parser')
@@ -32,7 +33,12 @@ def stock_list():
             break
         company_name = tags.text
         company_code = tags['href'].split('=')[1]
-        print(company_name, company_code)
+        # print(company_name, company_code)
+        company = Company()
+        company.company_code = company_code
+        company.company_name = company_name
+
+        company.save()
 
     num = 0
     for tags in stock_now:
@@ -40,9 +46,7 @@ def stock_list():
         if num == 11:
             break
         company_stock = tags.text
-        print(company_stock)
-
-
-
-
-stock_list()
+        # print(company_stock)
+        stock = Stock()
+        stock.company_stock = company_stock
+        stock.save()
